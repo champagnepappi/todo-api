cc6b37809404b499100e73f5313426a0ad07b13f
kevin <kevinochieng548@gmail.com>
2018-03-01 17:02:22 +0300

    define the delete_task method

--- app.py
+++ app.py
@@ -52,6 +52,13 @@ def update_task(task_id):
     task[0]['done'] = request.json.get('done', task[0]['done'])
     return jsonify({'task': task[0]})
 
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
